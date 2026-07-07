a4b1122b6f716e9ca7c7d025267597f8f0124e6c
stasz <staszekwindows@gmail.com>
2026-07-07 16:11:09 +0200

    Task edition added

--- script.js
+++ script.js
@@ -34,6 +34,10 @@ function createTask(task) {
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.textContent = "Delete";
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+
   span.textContent = task.text;
   if (task.completed) {
     span.classList.add("completed");
@@ -41,6 +45,8 @@ function createTask(task) {
 
   taskItem.appendChild(span);
   taskItem.appendChild(button);
+  taskItem.appendChild(editButton);
+
   taskList.appendChild(taskItem);
   span.addEventListener("click", function () {
     task.completed = !task.completed;
@@ -55,6 +61,34 @@ function createTask(task) {
     saveTasks();
     renderTasks();
   });
+
+  editButton.addEventListener("click", function () {
+    const text = span.textContent;
+    const field = document.createElement("input");
+    field.value = text;
+    field.focus();
+    taskItem.removeChild(span);
+    taskItem.appendChild(field);
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "save";
+    taskItem.appendChild(saveButton);
+    taskItem.removeChild(editButton);
+    saveButton.addEventListener("click", function () {
+      span.textContent = field.value;
+      taskItem.appendChild(span);
+      task.text = field.value;
+      saveTasks();
+      renderTasks();
+    });
+    //editTask(taskItem, task.text);
+  });
+}
+
+function editTask(item, text) {
+  const field = document.createElement("input");
+  field.textContent = text;
+  field.focus();
+  item.appendChild(field);
 }
 
 function saveTasks() {
@@ -81,7 +115,7 @@ function renderTasks() {
   }
 
   filteredTasks.forEach((task) => {
-    createTask(task, index);
+    createTask(task);
   });
   renderStatistics();
 }
