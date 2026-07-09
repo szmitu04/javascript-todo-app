02f990f8a92bb986708d2fc82f36055ad6836dd3
stasz <staszekwindows@gmail.com>
2026-07-09 14:16:47 +0200

    Changed colors, added active buttons, fixed layout

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const completedBtn = document.querySelector("#completedBtn");
 const totalTasks = document.querySelector("#totalAmnt");
 const remainingTasks = document.querySelector("#remainingAmnt");
 const completedTasks = document.querySelector("#completedAmnt");
+const filterButtons = document.querySelectorAll(".filter-btn");
 
 let tasks = [];
 let currentFilter = "all";
@@ -32,9 +33,14 @@ function createTask(task) {
   const taskItem = document.createElement("li");
   taskItem.classList.add("task-item");
   const span = document.createElement("span");
+
+  const taskContent = document.createElement("div");
+  taskContent.classList.add("task-content");
+  const taskActions = document.createElement("div");
+  taskActions.classList.add("task-actions");
+
   const button = document.createElement("button");
   button.textContent = "Delete";
-
   const editButton = document.createElement("button");
   editButton.textContent = "Edit";
 
@@ -43,9 +49,12 @@ function createTask(task) {
     span.classList.add("completed");
   }
 
-  taskItem.appendChild(span);
-  taskItem.appendChild(button);
-  taskItem.appendChild(editButton);
+  taskContent.appendChild(span);
+  taskActions.appendChild(button);
+  taskActions.appendChild(editButton);
+
+  taskItem.appendChild(taskContent);
+  taskItem.appendChild(taskActions);
 
   taskList.appendChild(taskItem);
   span.addEventListener("click", function () {
@@ -55,40 +64,42 @@ function createTask(task) {
   });
 
   button.addEventListener("click", function () {
-    const removedList = tasks.findIndex((t) => t === task); //szuka objektu w liscie objektów który jest klikanym objektem
+    const taskIndex = tasks.findIndex((t) => t === task); //szuka objektu w liscie objektów który jest klikanym objektem
 
-    tasks.splice(removedList, 1);
+    tasks.splice(taskIndex, 1);
     saveTasks();
     renderTasks();
   });
 
   editButton.addEventListener("click", function () {
-    const text = span.textContent;
-    const field = document.createElement("input");
-    field.value = text;
-    field.focus();
-    taskItem.removeChild(span);
-    taskItem.appendChild(field);
-    const saveButton = document.createElement("button");
-    saveButton.textContent = "save";
-    taskItem.appendChild(saveButton);
-    taskItem.removeChild(editButton);
-    saveButton.addEventListener("click", function () {
-      span.textContent = field.value;
-      taskItem.appendChild(span);
-      task.text = field.value;
-      saveTasks();
-      renderTasks();
-    });
+    editTask(task, span, taskContent, editButton, taskActions);
     //editTask(taskItem, task.text);
   });
 }
 
-function editTask(item, text) {
+function editTask(task, span, taskContent, editButton, taskActions) {
   const field = document.createElement("input");
-  field.textContent = text;
+  field.value = task.text;
   field.focus();
-  item.appendChild(field);
+
+  taskContent.replaceChild(field, span);
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+
+  taskActions.replaceChild(saveButton, editButton);
+
+  field.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      task.text = field.value;
+      saveTasks();
+      renderTasks();
+    }
+  });
+  saveButton.addEventListener("click", function () {
+    task.text = field.value;
+    saveTasks();
+    renderTasks();
+  });
 }
 
 function saveTasks() {
@@ -118,25 +129,31 @@ function renderTasks() {
     createTask(task);
   });
   renderStatistics();
+  updateFilterButtons();
 }
 
-allBtn.addEventListener("click", function () {
-  currentFilter = "all";
-  renderTasks();
-});
-
-activeBtn.addEventListener("click", function () {
-  currentFilter = "active";
-  renderTasks();
-});
-
-completedBtn.addEventListener("click", function () {
-  currentFilter = "completed";
-  renderTasks();
+filterButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    currentFilter = button.dataset.filter;
+    renderTasks();
+  });
 });
 
 function renderStatistics() {
   totalTasks.textContent = tasks.length;
   remainingTasks.textContent = tasks.filter((task) => !task.completed).length;
   completedTasks.textContent = tasks.filter((task) => task.completed).length;
+}
+
+function updateFilterButtons() {
+  filterButtons.forEach((button) => button.classList.remove("active"));
+
+  if (currentFilter === "all") {
+    allBtn.classList.add("active");
+  } else if (currentFilter === "active") {
+    activeBtn.classList.add("active");
+  } else {
+    // wiem że można by było użyć else if, ale w tym przypadku else wystarczy, bo mamy tylko 3 opcje
+    completedBtn.classList.add("active");
+  }
 }
